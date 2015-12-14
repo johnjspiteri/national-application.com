585b64817ace3887f73518ee1a1f262769193eda
Navpreet Singh <navpreet_gagar@live.com>
2015-12-14 13:07:44 -0500

    Map Style and Functionalty Added

--- client/js/common/common.controller.js
+++ client/js/common/common.controller.js
@@ -1,14 +1,43 @@
 (function() {
     'use strict';
 
-    function CommonController () {
+    function CommonController ($scope, uiGmapGoogleMapApi) {
+        $scope.map = {
+            center:
+                { latitude: 43.648125, longitude: -79.394845 },
+            zoom: 15
+         };
 
+        $scope.options = {
+            scrollwheel: false,
+            mapTypeControl: false,
+            streetViewControl: true,
+            minZoom: 17,
+            maxZoom: 17,
+            disableDefaultUI: false,
+            zoomControl: false,
+            draggable: false
+         };
+        $scope.marker = {
+            id: 1,
+            coords: {
+                latitude: 43.648125,
+                longitude: -79.394845,
+            },
+            options: {
+                draggable: false,
+            }
+        };
+
+        uiGmapGoogleMapApi.then(function(maps) {
+
+        });
     }
 
     angular
         .module('app.common')
         .controller('CommonController', CommonController);
 
-    CommonController.$inject = [];
+    CommonController.$inject = ['$scope', 'uiGmapGoogleMapApi'];
 
 })();
