bcddaced7d1c9bcdbe6c4610d35e8fe611463802
John Spiteri <john@johnspiteri.io>
2015-12-21 15:02:49 +0100

    Updated Questions Style

--- client/js/question/question.controller.js
+++ client/js/question/question.controller.js
@@ -10,17 +10,17 @@
         $scope.answers= [{
             title: "Once I create my card how do I use it?",
             content: "YES, EACH FAMILY MEMBER NEEDS TO GO BACK TO THE WEBSITE AND CREATE A NEW CARD FOR EACH MEMBER OF THE FAMILY. HOWEVER, OTHER FAMILY MEMBERS AND FRIENDS MAY USE YOUR DISCOUNT COUPON CARD. ",
-            keyword: [{
-                value: "john",
+            keywords: [{
+                value: "Test",
             },{
-                value: "Hungry",
+                value: "Profit",
             },{
-                value: "Sick",
+                value: "Price",
             },{
                 value: "Pet"
             }]
         }, {
-            title: "WHAT IF I CHANGE PHARMACIES?",
+            title: "What If I Change Pharmacies?",
             content: "YES, EACH FAMILY MEMBER NEEDS TO GO BACK TO THE WEBSITE AND CREATE A NEW CARD FOR EACH MEMBER OF THE FAMILY. HOWEVER, OTHER FAMILY MEMBERS AND FRIENDS MAY USE YOUR DISCOUNT COUPON CARD. ",
             keywords:[{
                 value: "Drug",
@@ -32,7 +32,7 @@
                 value: "Pet"
             }]
         },{
-            title: "DOES EVERY MEMBER OF MY FAMILY NEED A CARD?",
+            title: "Does Every Family Member Need A Card?",
             content: "YES, EACH FAMILY MEMBER NEEDS TO GO BACK TO THE WEBSITE AND CREATE A NEW CARD FOR EACH MEMBER OF THE FAMILY. HOWEVER, OTHER FAMILY MEMBERS AND FRIENDS MAY USE YOUR DISCOUNT COUPON CARD. ",
             keywords:[{
                 value: "Test",
@@ -44,8 +44,8 @@
                 value: "Pet"
             }]
         },{
-            title: "HOW MUCH DOES THIS PROGRAM COST ME?",
-            content: "WE PROVIDE YOU WITH THE FREE YOUR RXCARD.COM COUPON CARD AT NO COST. WE PROVIDE THIS SERVICE TO ANY AND ALL AMERICANS THAT WISH TO SAVE ON THEIR PRESCRIPTION PRICES.  ",
+            title: "How Much Does This Program Cost Me?",
+            content: "YES, EACH FAMILY MEMBER NEEDS TO GO BACK TO THE WEBSITE AND CREATE A NEW CARD FOR EACH MEMBER OF THE FAMILY. HOWEVER, OTHER FAMILY MEMBERS AND FRIENDS MAY USE YOUR DISCOUNT COUPON CARD. ",
             keywords:[{
                 value: "Drug",
             },{
@@ -56,8 +56,8 @@
                 value: "Pet"
             }]
         },{
-            title: "WHICH PHARMACIES CAN I GO TO?",
-            content: 'WE HAVE OVER 63,000 PARTICIPATING MAJOR CHAINS PHARMACIES AND LOCAL PHARMACIES NATIONWIDE, PLEASE GO BACK TO THE WEBSITE AND CLICK ON THE "PHARMACY LOCATOR". Q: WHAT TYPE OF DISCOUNT CAN I EXPECT? A: DISCOUNTS VARY AND CAN RANGE ANYWHERE FROM 0%-75% DEPENDING ON THE TYPE OF PRESCRIPTION, BRAND NAME, GENERIC NAME AND THE LOCATION OF THE SPECIFIC PHARMACY. THE DISCOUNTS ARE BASED ON THE COMBINED PURCHASING POWER OF THE MILLIONS OF CLIENTS OF OUR NATIONAL PHARMACY BENEFITS MANAGER. (PBM) Q: WHERE CAN I FIND OUT THE APPROXIMATE COST OF THE PRESCRIPTION DRUGS? A: PLEASE GO BACK TO THE WEBSITE AND CLICK ON THE "MEDICATION PRICING TOOL"',
+            title: "Which Pharmacies Can I Go To?",
+            content: "YES, EACH FAMILY MEMBER NEEDS TO GO BACK TO THE WEBSITE AND CREATE A NEW CARD FOR EACH MEMBER OF THE FAMILY. HOWEVER, OTHER FAMILY MEMBERS AND FRIENDS MAY USE YOUR DISCOUNT COUPON CARD. ",
             keywords:[{
                 value: "Test",
             },{
