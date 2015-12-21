e90a2ec253f4e1b4bdd565696306d356e10fb8e9
John Spiteri <john@johnspiteri.io>
2015-12-21 18:38:32 +0100

    Integrated About-Us Contact Form & Map

--- client/js/about/about.controller.js
+++ client/js/about/about.controller.js
@@ -1,11 +1,43 @@
 (function() {
     'use strict';
 
-    function About ($scope) {
+    function About ($scope, uiGmapGoogleMapApi) {
 
         $scope.contact = true;
 
-        
+        $scope.toggle = function() {
+            $scope.contact = !$scope.contact;
+        };
+        $scope.map = {
+            center: {
+                latitude: 43.648125,
+                longitude: -79.394845
+            },
+            zoom: 14
+        };
+        $scope.options = {
+            disableDefaultUI: false,
+            draggable: false,
+            mapTypeControl: false,
+            maxZoom: 14,
+            minZoom: 14,
+            scrollwheel: false,
+            streetViewControl: false,
+            zoomControl: false,
+        };
+        $scope.marker = {
+            id: 1,
+            coords: {
+                latitude: 43.648125,
+                longitude: -79.394845,
+            },
+            options: {
+                draggable: false,
+                icon: '/img/car-icon.png',
+            }
+        };
+
+        uiGmapGoogleMapApi.then(function(maps) {});
 
     }
 
@@ -13,6 +45,6 @@
         .module('app')
         .controller('About', About);
 
-    About.$inject = ['$scope'];
+    About.$inject = ['$scope', 'uiGmapGoogleMapApi'];
 
 })();
