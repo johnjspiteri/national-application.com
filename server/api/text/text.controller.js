db36cf357780265bb63060c9e7d705daa78797b2
John Spiteri <john@johnspiteri.io>
2016-01-03 00:15:23 +0100

    Updated Twilio Account Information

--- server/api/text/text.controller.js
+++ server/api/text/text.controller.js
@@ -2,14 +2,14 @@
     'use strict';
 
     var twilio = require('twilio');
-    var accountSid = 'ACd2197fa58583c4cc66672724cd292884';
-    var authToken = 'c6173fe5bc8d76e8950c5eedf123a0b6';
+    var accountSid = 'MG600b0769322aa0618f247699177c4e51';
+    var authToken = 'f0623ddc03f6cfe67291bec7e6289f84';
     var client = require('twilio')(accountSid, authToken);
 
     exports.create = function (req, res) {
         client.messages.create({
             to: req.body.destination,
-            from:'+14352619004',
+            from:'+14352144586',
             body:'Here is your National Rx Card, you can use this card at over 63,000 pharmacies nationwide. If you have any questions of comments please contact us at 1-866-444-5555.',
             mediaUrl: req.body.file,
         }, function(error, message) {
