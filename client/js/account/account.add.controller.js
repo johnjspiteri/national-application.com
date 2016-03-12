411294ebf5f512439874c517154c0fbe8305753b
John Spiteri <john@johnspiteri.io>
2016-03-12 18:10:51 -0500

    Updated Account Functions

--- client/js/account/account.add.controller.js
+++ client/js/account/account.add.controller.js
@@ -1,58 +1,58 @@
 (function() {
-    'use strict';
+	'use strict';
 
-    function Add ($document, $scope, $state, account) {
+	function Add ($document, $scope, $state, account) {
 
-        $scope.readOnly = true;
+		$scope.readOnly = true;
 
-        $scope.createClean = function() {
-            $scope.item.clean = ($scope.item.organization).replace(/[\s:,!?]/g, '-').replace(/--/g, '-').replace(/-$/, '').toLowerCase();
+		$scope.createClean = function() {
+			$scope.item.clean = ($scope.item.organization).replace(/[\s:,!?]/g, '-').replace(/--/g, '-').replace(/-$/, '').toLowerCase();
 
-            if($scope.item.organization) {
-                $scope.url = "http://nationalrxcard.com/nrx/" + $scope.item.clean;
-            }
-        };
+			if($scope.item.organization) {
+				$scope.item.url = "http://nationalrxcard.com/nrx/" + $scope.item.clean;
+			}
+		};
 
-        $scope.item = {
-            file_url: 'https://s3.amazonaws.com/nationalrx/card/',
-            plan: {
-                bin: '018398',
-                pcn: '48300712',
-                group: '96100245',
-            }
-        };
+		$scope.item = {
+			file_url: 'https://s3.amazonaws.com/nationalrx/card/',
+			plan: {
+				bin: '018398',
+				pcn: '48300712',
+				group: '96100245',
+			}
+		};
 
-        $scope.addItem = function() {
-            $scope.createClean();
+		$scope.updateImageState = function () {
+			$scope.item.imageState = !$scope.item.imageState;
+		};
 
-            account.create({
-                clean: $scope.item.clean,
-                image: $scope.item.image,
-                imageState: $scope.item.imageState,
-                plan: {
-                    bin: $scope.item.plan.bin,
-                    pcn: $scope.item.plan.pcn,
-                    group: $scope.item.plan.group,
-                    patient_id: $scope.item.plan.patient_id,
-                },
-                organization: $scope.item.organization,
-                email: $scope.item.email,
-                file_url: $scope.item.file_url
+		$scope.addItem = function() {
+			$scope.createClean();
+			account.create({
+				clean: $scope.item.clean,
+				image: $scope.item.image,
+				imageState: $scope.item.imageState,
+				plan: {
+					bin: $scope.item.plan.bin,
+					pcn: $scope.item.plan.pcn,
+					group: $scope.item.plan.group,
+					patient_id: $scope.item.plan.patient_id,
+				},
+				organization: $scope.item.organization,
+				email: $scope.item.email,
+				file_url: $scope.item.file_url
+			}).$promise.then(function() {
+				$scope.item = {};
+				$scope.item.image = {};
+				$state.go('backend.list', {}, {reload: true});
+			});
+		};
+	}
 
+	angular
+		.module('app.account')
+		.controller('Add', Add);
 
-            });
-            // initialize
-            $scope.item = {};
-            $scope.item.image = {};
-            $scope.url = {};
-            $state.go('backend.list');
-        };
-    }
-
-    angular
-        .module('app.account')
-        .controller('Add', Add);
-
-    Add.$inject = ['$document', '$scope', '$state', 'account'];
+	Add.$inject = ['$document', '$scope', '$state', 'account'];
 
 })();
--- client/js/account/account.edit.controller.js
+++ client/js/account/account.edit.controller.js
@@ -1,73 +1,85 @@
 (function() {
-    'use strict';
+	'use strict';
 
-    function Edit ($document, $scope, $state, account, itemResolve) {
+	function Edit ($document, $scope, $state, account, itemResolve) {
 
-        $scope.item = itemResolve;
-        console.log($scope.item);
+		$scope.item = itemResolve;
 
-        $scope.url = "http://nationalrxcard.com/nrx/" + $scope.item.clean;
+		$scope.url = "http://nationalrxcard.com/nrx/" + $scope.item.clean;
 
-        $scope.saveImage = function(content) {
-            account.update({id:$scope.item._id}, {$set: {
-                'image': content,
-            }}).$promise.then(function(data){
-                $scope.item.image = data.image;
-            });
-            $scope.new = null;
-        };
-        $scope.updateImageState = function () {
-            $scope.item.imageState = !$scope.item.imageState;
-            account.update({id:$scope.item._id}, {
-                'imageState': $scope.item.imageState
-            });
-        };
-        $scope.updateOrganization = function () {
-            account.update({id:$scope.item._id}, {
-                'organization': $scope.item.organization
-            });
-        };
-        $scope.updateEmail = function () {
-            account.update({id:$scope.item._id}, {
-                'email': $scope.item.email
-            });
-        };
-        $scope.updateURL = function () {
-            account.update({id:$scope.item._id}, {
-                'file_url': $scope.item.file_url
-            });
-        };
-        $scope.updateBIN = function () {
-            account.update({id:$scope.item._id}, {
-                'plan.bin': $scope.item.plan.bin
-            });
-        };
-        $scope.updatePCN = function () {
-            account.update({id:$scope.item._id}, {
-                'plan.pcn': $scope.item.plan.pcn
-            });
-        };
-        $scope.updateGroup = function () {
-            account.update({id:$scope.item._id}, {
-                'plan.group': $scope.item.plan.group
-            });
-        };
-        $scope.updatePatientID = function () {
-            account.update({id:$scope.item._id}, {
-                'plan.patient_id': $scope.item.plan.patient_id
-            });
-        };
+		$scope.createClean = function() {
+			$scope.item.clean = ($scope.item.organization).replace(/[\s:,!?]/g, '-').replace(/--/g, '-').replace(/-$/, '').toLowerCase();
 
-        $scope.deleteItem = function (data) {
-            account.destroy({id:data});
-            $state.go('backend.list', {}, {reload: true});
-        };
-    }
+			if($scope.item.organization) {
+				$scope.url = "http://nationalrxcard.com/nrx/" + $scope.item.clean;
+			}
+		};
 
-    angular
-        .module('app.account')
-        .controller('Edit', Edit);
+		$scope.saveImage = function(content) {
+			account.update({id:$scope.item._id}, {$set: {
+				'image': content,
+			}}).$promise.then(function(data){
+				$scope.item.image = data.image;
+			});
+			$scope.new = null;
+		};
+		$scope.updateImageState = function () {
+			$scope.item.imageState = !$scope.item.imageState;
+			account.update({id:$scope.item._id}, {
+				'imageState': $scope.item.imageState
+			});
+		};
 
-    Edit.$inject = ['$document', '$scope', '$state', 'account', 'itemResolve'];
+		$scope.updateOrganization = function () {
+			$scope.createClean();
+			account.update({id:$scope.item._id}, {
+				'clean': $scope.item.clean,
+				'organization': $scope.item.organization
+			});
+			$state.go($state.current, {id: $scope.item.clean}, {notify: false});
+
+		};
+		$scope.updateEmail = function () {
+			account.update({id:$scope.item._id}, {
+				'email': $scope.item.email
+			});
+		};
+		$scope.updateURL = function () {
+			account.update({id:$scope.item._id}, {
+				'file_url': $scope.item.file_url
+			});
+		};
+		$scope.updateBIN = function () {
+			account.update({id:$scope.item._id}, {
+				'plan.bin': $scope.item.plan.bin
+			});
+		};
+		$scope.updatePCN = function () {
+			account.update({id:$scope.item._id}, {
+				'plan.pcn': $scope.item.plan.pcn
+			});
+		};
+		$scope.updateGroup = function () {
+			account.update({id:$scope.item._id}, {
+				'plan.group': $scope.item.plan.group
+			});
+		};
+		$scope.updatePatientID = function () {
+			account.update({id:$scope.item._id}, {
+				'plan.patient_id': $scope.item.plan.patient_id
+			});
+		};
+
+		$scope.deleteItem = function (data) {
+			account.destroy({id:data});
+			$state.go('backend.list', {}, {reload: true});
+		};
+	}
+
+	angular
+		.module('app.account')
+		.controller('Edit', Edit);
+
+	Edit.$inject = ['$document', '$scope', '$state', 'account', 'itemResolve'];
 
 })();
